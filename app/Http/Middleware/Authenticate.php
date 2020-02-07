94948868ad5b6c6f70317496f905899b66f03e61
kevinfflxo <kevin0507a@gmail.com>
2020-02-07 18:58:46 +0800

    tweak Authenticate middleware without modify illuminate unauthenticated

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Auth\AuthenticationException;
 
 class Authenticate extends Middleware
 {
@@ -12,10 +13,32 @@ class Authenticate extends Middleware
      * @param  \Illuminate\Http\Request  $request
      * @return string|null
      */
+
+    public $login;
+
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('login');
+            return route($this->login);
         }
+    }
+
+    protected function unauthenticated($request, array $guards)
+    {   
+        $guard = $guards[0];
+
+        switch ($guard) {
+            case 'admin':
+                $this->login = 'admin.login';
+                break;
+            
+            default:
+                $this->login = 'login';
+                break;
+        }
+
+        throw new AuthenticationException(
+            'Unauthenticated.', $guards, $this->redirectTo($request)
+        );
     }
 }
